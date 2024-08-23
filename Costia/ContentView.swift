ba41f82808da23911485be68308c662c6f233f98
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2024-08-23 15:45:26 +0300

    Initial Commit

---
+++ Costia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Costia
+//
+//  Created by R. Metehan GÖKTAŞ on 23.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Costia/CostiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CostiaApp.swift
+//  Costia
+//
+//  Created by R. Metehan GÖKTAŞ on 23.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct CostiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
