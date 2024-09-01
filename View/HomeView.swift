4c0241e70596e6072e4a21f181354aba26193ae1
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2024-09-01 19:35:28 +0300

    fix

--- View/HomeView.swift
+++ View/HomeView.swift
@@ -2,38 +2,10 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        TabView {
-            ScanView()
-                .tabItem {
-                    Label("Scan", systemImage: "magnifyingglass")
-                }
-        }
-    }
-}
-
-struct ScanView: View {
-    var body: some View {
-        VStack {
-            Text("Scan View")
-                .font(.largeTitle)
-                .padding()
-            Spacer()
-        }
-    }
-}
-
-struct MyListView: View {
-    var body: some View {
-        VStack {
-            Text("My List View")
-                .font(.largeTitle)
-                .padding()
-            Spacer()
-        }
+        Text("home")
     }
 }
 
 #Preview {
     HomeView()
 }
-
