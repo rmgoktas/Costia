af51560957958d355ca928fc83f916ad3d5e2335
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2024-10-30 03:55:15 +0300

    data binding from vm, removed webkit func

--- View/ProductSearchView.swift
+++ View/ProductSearchView.swift
@@ -1,115 +1,167 @@
 import SwiftUI
-import WebKit
 
 struct ProductSearchView: View {
-    
-    @State private var searchText: String
-    @Environment(\.presentationMode) var presentationMode
-    @State private var isLoading: Bool = true
-    @State private var loadError: Bool = false
+    @StateObject private var viewModel: ProductSearchViewModel
     @State private var showLoadingScreen: Bool = true
-    @State private var showAlternateText: Bool = false
+    @State private var showingAddItemView = false
+    @State private var selectedMarket: Market?
+    @State private var selectedQuantity: Int = 1
     
     init(searchText: String) {
-        self._searchText = State(initialValue: searchText)
+        _viewModel = StateObject(wrappedValue: ProductSearchViewModel(searchText: searchText))
     }
     
     var body: some View {
         NavigationView {
             ZStack {
                 if showLoadingScreen {
-                    VStack {
-                        ProgressView()
-                            .progressViewStyle(CircularProgressViewStyle())
-                            .scaleEffect(2)
-                        if !showAlternateText {
-                            Text("Sizin için en uygun fiyatları buluyoruz...")
-                                .font(.headline)
-                                .padding(.top, 40)
-                                .transition(.opacity)
-                                .animation(.easeInOut(duration: 2.0), value: showAlternateText)
-                        }
-                        if showAlternateText {
-                            Text("En iyi fırsatları yakalayın!")
-                                .font(.headline)
-                                .padding(.top, 40)
-                                .transition(.opacity)
-                                .animation(.easeInOut(duration: 2.0), value: showAlternateText)
-                        }
-                        Text("Lütfen bekleyin.")
-                            .font(.subheadline)
-                            .padding(.top, 10)
-                            .transition(.opacity)
-                            .animation(.easeInOut(duration: 2.0).delay(1.0), value: showLoadingScreen)
-                    }
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                            withAnimation {
-                                showAlternateText = true
-                            }
-                        }
-                        // 5 saniye delay
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
-                            withAnimation {
-                                showLoadingScreen = false
-                            }
-                        }
-                    }
+                    loadingView
                 } else {
-                    VStack {
-                        Spacer()
-                            .frame(height: 100)
-                        
-                        if isLoading {
-                            ProgressView("Sonuçlar yükleniyor...")
-                                .padding()
-                                .scaleEffect(1.2)
-                        } else if loadError {
-                            Text("Bir hata oluştu. Lütfen tekrar deneyin.")
-                                .foregroundColor(.red)
-                                .transition(.opacity)
-                        } else {
-                            WebView(url: URL(string: "https://www.google.com/search?tbm=shop&q=\(searchText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")")!)
-                                .frame(height: 750)
-                                .transition(.slide)
-                                .animation(.spring(), value: isLoading)
-                        }
-                        
-                        Spacer()
-                    }
-                    .navigationTitle("En İyi Eşleşmeler")
-                    .navigationBarItems(leading: Button(action: {
-                        withAnimation {
-                            presentationMode.wrappedValue.dismiss()
-                        }
-                    }) {
-                        HStack {
-                            Image(systemName: "chevron.left")
-                            Text("Geri")
-                        }
-                        .foregroundColor(.blue)
-                        .padding(.leading, 10)
-                        .cornerRadius(8)
-                        .shadow(radius: 5)
-                    })
-                    .onAppear {
-                        if URL(string: "https://www.google.com/search?tbm=shop&q=\(searchText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")") == nil {
-                            loadError = true
-                        }
-                        isLoading = false
-                    }
+                    productListView
                 }
             }
-            .transition(.move(edge: .bottom))
-            .animation(.spring(), value: isLoading)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    showLoadingScreen = false
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("En İyi Eşleşmeler")
+                    
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                }
+            }
         }
+        .sheet(isPresented: $showingAddItemView) {
+            addItemView
+                .presentationDetents([.height(400)])
+        }
+    }
+    
+    private var loadingView: some View {
+        VStack {
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle())
+                .scaleEffect(2)
+            Text("Sizin için en uygun fiyatları buluyoruz...")
+                .font(.headline)
+                .padding(.top, 40)
+            Text("Lütfen bekleyin.")
+                .font(.subheadline)
+                .padding(.top, 10)
+        }
+    }
+    
+    private var productListView: some View {
+        Group {
+            if viewModel.isLoading {
+                ProgressView("Sonuçlar yükleniyor...")
+                    .padding()
+                    .scaleEffect(1.2)
+            } else if viewModel.loadError {
+                Text("Bir hata oluştu. Lütfen tekrar deneyin.")
+                    .foregroundColor(.red)
+            } else {
+                List(viewModel.products) { product in
+                    VStack(alignment: .leading) {
+                        AsyncImage(url: URL(string: product.imageURL)) { image in
+                            image.resizable()
+                                 .scaledToFit()
+                                 .frame(height: 150)
+                                 .cornerRadius(10)
+                                 .padding(.horizontal)
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        
+                        Text(product.name)
+                            .font(.title)
+                            .padding(.bottom, 5)
+                        
+                        ForEach(product.markets, id: \.name) { market in
+                            HStack {
+                                Text(market.name)
+                                Spacer()
+                                Text("\(market.price, specifier: "%.2f")₺")
+                                    .foregroundColor(market.price == viewModel.cheapestPrice ? .green : .gray)
+                                    .font(market.price == viewModel.cheapestPrice ? .title2 : .body)
+                                
+                                Button(action: {
+                                    selectedMarket = market
+                                    showingAddItemView = true
+                                }) {
+                                    Image(systemName: "chevron.down")
+                                }
+                                .buttonStyle(PlainButtonStyle())
+                                .padding(.leading)
+                            }
+                            .padding(.vertical, 5)
+                        }
+                    }
+                    .padding()
+                }
+            }
+        }
+    }
+
+    private var addItemView: some View {
+        VStack {
+            Text("Sepete Ekle")
+                .font(.headline)
+                .padding()
+            
+            HStack {
+                Text("Market: \(selectedMarket?.name ?? "")")
+                    .padding()
+                Spacer()
+                Text("\(selectedMarket?.price ?? 0, specifier: "%.2f")₺")
+                    .font(.title2)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            
+            Stepper("Adet: \(selectedQuantity)", value: $selectedQuantity, in: 1...99)
+                .padding()
+
+            HStack {
+                Button("Kapat") {
+                    showingAddItemView = false
+                }
+                .padding()
+                .background(Color.red.opacity(0.2))
+                .cornerRadius(10)
+                
+                Spacer()
+                
+                Button("Ekle") {
+                    if let market = selectedMarket {
+                        let item = CartItem(image: UIImage(named: "placeholder") ?? UIImage(), name: market.name, quantity: selectedQuantity, unitPrice: market.price)
+                        CartViewModel().addItem(item)
+                    }
+                    showingAddItemView = false
+                }
+                .padding()
+                .background(Color.blue.opacity(0.2))
+                .cornerRadius(10)
+            }
+            .padding()
+        }
+        .frame(maxHeight: .infinity)
+        .frame(maxWidth: .infinity)
+        .background(Color(UIColor.systemGray5))
+        .cornerRadius(12)
+        .shadow(radius: 20)
+        .padding(.horizontal)
     }
 }
 
+
 struct ProductSearchView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductSearchView(searchText: "beypazarı maden suyu")
+        ProductSearchView(searchText: "123456789")
             .previewLayout(.sizeThatFits)
-            .preferredColorScheme(.light) //dark mod için
+            .preferredColorScheme(.light)
     }
 }
