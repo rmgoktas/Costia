a5599960e039a3cbec40aa69b176af38bfbc6f68
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2024-11-01 00:41:08 +0300

    added cartviewmodel as an environmentalObj

--- Costia/CostiaApp.swift
+++ Costia/CostiaApp.swift
@@ -3,11 +3,13 @@ import SwiftUI
 @main
 struct CostiaApp: App {
     @StateObject private var scannerViewModel = ScannerViewModel()
+    @StateObject private var cartViewModel = CartViewModel()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(scannerViewModel)
+                .environmentObject(cartViewModel)
         }
     }
 }
